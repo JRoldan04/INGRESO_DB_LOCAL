155fa5bfc48a564923364e35cce45a7731128980
JRoldan04 <chingal20@gmail.com>
2025-03-17 19:53:06 -0500

    ACTUALIZACION

--- SQLExpress.py
+++ SQLExpress.py
@@ -3,11 +3,13 @@ import pandas as pd
 
 # DATOS DE CONEXION
 
+
+
 try:
     connection = pyodbc.connect('DRIVER={SQL SERVER};SERVER=10.1.21.237\SQLEXPRESS;DATABASE=CheckPostingDB;UID=db_user;PWD=Or4cl3#1')
     print("Conexion Exitosa")
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM ALLERGEN;")
+    cursor.execute("SELECT * FROM CHECKS;")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
